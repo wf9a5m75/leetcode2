caf14f8e59f3807b6486b3ddb8eb951437f7b1a6
masashi <wf9a5m75@gmail.com>
2022-06-20 08:32:35 -0700

    344.Reverse String

---
+++ 344.reverse-string/README.md
@@ -0,0 +1,4 @@
+# 344. Reverse String (Recursion I)
+## level: Easy
+
+https://leetcode.com/problems/reverse-string/
---
+++ 344.reverse-string/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        L = 0
+        R = len(s) - 1
+        while(L < R):
+            s[L], s[R] = s[R], s[L]
+            L += 1
+            R -= 1
+
+        
