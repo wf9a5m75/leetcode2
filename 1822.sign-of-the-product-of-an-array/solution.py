a880e08699936f08d75dccaae2a00b1f906ed420
masashi <wf9a5m75@gmail.com>
2022-07-05 10:24:10 -0700

    1822. Sign of the Product of an Array (Programming Skills I)

---
+++ 1822.sign-of-the-product-of-an-array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        sign = 1
+        for num in nums:
+            if num < 0:
+                sign *= -1
+            elif num == 0:
+                return 0
+        return sign
