b2d600f57fb9819e5f42b7aed5edd65ca6edacea
masashi <wf9a5m75@gmail.com>
2022-07-06 11:02:54 -0700

    1873. Calculate Special Bonus (SQL I)

---
+++ 1837.calculate-special-bonus/README.md
@@ -0,0 +1,4 @@
+# 1873. Calculate Special Bonus (SQL I)
+## level: Easy
+
+https://leetcode.com/problems/calculate-special-bonus/
---
+++ 1837.calculate-special-bonus/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    employee_id,
+    IF((employee_id % 2 = 1) and (name NOT LIKE "M%"), salary, 0) as bonus
+FROM
+    Employees
+ORDER BY
+    employee_id
