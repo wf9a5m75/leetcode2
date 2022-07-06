410fc24611290cc772483c742edd36272e902981
masashi <wf9a5m75@gmail.com>
2022-07-06 11:25:35 -0700

    196. Delete Duplicate Emails (SQL I)

---
+++ 196.delete-duplicate-emails/README.md
@@ -0,0 +1,4 @@
+# 196. Delete Duplicate Emails (SQL I)
+## level: Easy
+
+https://leetcode.com/problems/delete-duplicate-emails/
---
+++ 196.delete-duplicate-emails/solution.sql
@@ -0,0 +1,10 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE p1 FROM
+    Person p1
+INNER JOIN
+    Person p2
+WHERE
+    p1.id > p2.id AND
+    p1.email = p2.email
+    
