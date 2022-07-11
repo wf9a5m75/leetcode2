5862c86f4abef8293f302189aff7d12df0c27e07
masashi <wf9a5m75@gmail.com>
2022-07-11 15:22:48 -0700

    198. House Robber (Dynamic Programming I)

---
+++ 198.house-robber/README.md
@@ -0,0 +1,4 @@
+# 198. House Robber (Dynamic Programming I)
+## level: Medium
+
+https://leetcode.com/problems/house-robber/
---
+++ 198.house-robber/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        N = len(nums)
+        if (N == 1):
+            return nums[0]
+        if (N == 2):
+            return max(nums)
+
+        dp = [0] * N
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, N):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[N - 1]
