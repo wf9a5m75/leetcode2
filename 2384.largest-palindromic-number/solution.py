2f80ed308feb9886ff1f481613f24060e15e7d58
masashi <wf9a5m75@gmail.com>
2022-09-29 23:38:45 -0700

    2384. Largest Palindromic Number

--- 2384.largest-palindromic-number/solution.py
+++ 2384.largest-palindromic-number/solution.py
@@ -1,29 +1,52 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
-        counts = [0] * 10
-
+        # Bit flags for preventing the leading zeros.
+        # 0bBBBBBBBBBB
+        #   9876543210
         kinds = 0
+
+        # Counting up the number of digits
+        counts = [0] * 10
         for c in num:
             idx = int(c)
             counts[idx] += 1
+
+            # Sets 1 on the bit flag
             kinds |= 1 << idx
 
-        result = ""
+        # Find the center digit which is the largest digit and appears odd times.
+        mid = ""
         for i in range(9, -1, -1):
             if (counts[i] & 1) == 1:
-                if result == "":
-                    result = str(i)
+
+                # Pick the only the largest digit
+                if mid == "":
+                    mid = str(i)
+
+                # converts to even times
                 counts[i] -= 1
+
+                # If there is no "i" digit, clear the bit flag
                 if counts[i] == 0:
                     kinds = kinds ^ (1 << i)
 
-        if (kinds != 1):
+        # The each bit of the kinds variable denotes
+        # what digits are still available.
+        #
+        # If there is only "0" digit, kinds = 0b0000000001.
+        # If there is no digit, kinds = 0b0000000000
+        #
+        # Therefore, we check the digits from 2 to 9 using bit masking (0x3FE = 0b1111111110),
+        # then if it is greater than 0, it means there is something available.
+        prefix = ""
+        suffix = ""
+        if (kinds & 0x3FE) > 0:
             for i in range(10):
                 cnt = counts[i] >> 1
                 if cnt > 0:
-                    c = str(i)
-                    result = f"{c * cnt}{result}{c * cnt}"
-                    counts[i] = 0
+                    tmp = str(i) * cnt
+                    prefix = tmp + prefix
+                    suffix = suffix + tmp
 
-        result = "0" if result == "" else result
-        return result
+        result = prefix + mid + suffix
+        return "0" if result == "" else result
